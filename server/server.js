c10d14f539afaa7f3c3211a81c767d45373165b4
Niquimi <genasdenic@gmail.com>
2025-07-23 16:43:25 +0200

    feat: server canvas created

---
+++ server/server.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const http = require("http");
+const path = require("path");
+const { Server } = require("socket.io");
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+// Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+// Static files
+app.use(express.static(path.join(__dirname, "../public")));
+
+// Routes
+app.use('/', require('./routes/home'));
+
+io.on('connection', (socket) => {
+  console.log('A user connected');
+
+  socket.on("draw", (data) => {
+    socket.broadcast.emit("draw", data);
+  });
+
+  socket.on("clear", () => {
+    io.emit("clear");
+  });
+
+  socket.on('disconnect', () => {
+    console.log('User disconnected');
+  });
+
+});
+
+// Start the server
+server.listen(3000, () => {
+    console.log(`Server running on http://localhost:${3000}`);
+});
