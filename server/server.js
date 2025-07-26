1c670ebf7577993c977f66fe204e0c0585da063b
Niquimi <genasdenic@gmail.com>
2025-07-26 21:01:26 +0200

    feat: cursor display

--- server/server.js
+++ server/server.js
@@ -20,6 +20,43 @@ app.use('/', require('./routes/home'));
 io.on('connection', (socket) => {
   console.log('A user connected');
 
+  // --- CURSOR SHARING LOGIC START ---
+  // Fixed color palette
+  const cursorColors = [
+    '#e6194b', '#3cb44b', '#ffe119', '#4363d8', '#f58231', '#911eb4', '#46f0f0', '#f032e6', '#bcf60c', '#fabebe',
+    '#008080', '#e6beff', '#9a6324', '#fffac8', '#800000', '#aaffc3', '#808000', '#ffd8b1', '#000075', '#808080'
+  ];
+  // Store all users' cursors
+  if (!io.cursors) io.cursors = {};
+  if (!io.userCount) io.userCount = 0;
+
+  // Assign name and color
+  io.userCount++;
+  const userName = `User ${io.userCount}`;
+  const userColor = cursorColors[(io.userCount - 1) % cursorColors.length];
+  io.cursors[socket.id] = { x: null, y: null, name: userName, color: userColor };
+
+  // Send assigned name/color to client
+  socket.emit('cursor-init', { name: userName, color: userColor });
+
+  // Handle cursor move
+  socket.on('cursor-move', ({ x, y }) => {
+    if (io.cursors[socket.id]) {
+      io.cursors[socket.id].x = x;
+      io.cursors[socket.id].y = y;
+      // Broadcast all cursors to everyone
+      io.emit('cursors', io.cursors);
+    }
+  });
+
+  // On disconnect, remove cursor
+  socket.on('disconnect', () => {
+    delete io.cursors[socket.id];
+    io.emit('cursors', io.cursors);
+    console.log('User disconnected');
+  });
+  // --- CURSOR SHARING LOGIC END ---
+
   socket.on("draw", (data) => {
     socket.broadcast.emit("draw", data);
   });
@@ -30,10 +67,6 @@ io.on('connection', (socket) => {
 
   socket.on("clear", () => {
     io.emit("clear");
-  });
-
-  socket.on('disconnect', () => {
-    console.log('User disconnected');
   });
 
 });
